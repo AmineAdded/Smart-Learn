31288eb6da9cff81ca8831badf53cdbea5d7964b
KarouiSouha <karoui.souha18@gmail.com>
2025-11-30 01:03:45 +0100

    update video

--- Frontend/lib/config/api_config.dart
+++ Frontend/lib/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // URL de base de votre API
   // Remplacez cette URL par celle de votre tunnel Cloudflare
-  static const String baseUrl = 'https://accommodations-thomson-composed-aye.trycloudflare.com';
+  static const String baseUrl = 'https://macie-unprognosticative-kylan.ngrok-free.dev';
 
   // Timeout pour les requêtes HTTP (en secondes)
   static const int timeoutSeconds = 30;
